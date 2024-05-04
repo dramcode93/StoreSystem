09cb8784173e1e26b204afbb227d041503d1d12f
Ahmed-mohamed11 <ahmedmohamed0aa349@gmail.com>
2024-05-05 01:10:31 +0300

    updates

--- src/components/Category/CategoryTable.jsx
+++ src/components/Category/CategoryTable.jsx
@@ -2,14 +2,7 @@ import React, { useState, useEffect, useCallback, useRef } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
 import { CiSearch } from "react-icons/ci";
-import {
-  CaretLeft,
-  CaretRight,
-  DotsThree,
-  Eye,
-  NotePencil,
-  TrashSimple,
-} from "@phosphor-icons/react";
+import { CaretLeft, CaretRight, DotsThree, Eye, NotePencil, TrashSimple } from "@phosphor-icons/react";
 import { useI18nContext } from "../context/i18n-context";
 import Loading from "../Loading/Loading";
 import ConfirmationModal from "./ConfirmationModel";
@@ -25,19 +18,23 @@ const CategoryTable = ({ openEdit, openCreate, openPreview }) => {
   const [searchInput, setSearchInput] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
   const [pagination, setPagination] = useState({
-    currentPage: 1,
+    currentPge: 1,
     totalPages: 1,
+    totalRecords: 0,
   });
-
   const fetchData = useCallback(async () => {
     try {
       if (token) {
         const categoriesResponse = await axios.get(
-          `${API_category}?sort=category name&search=${searchTerm}&page=${pagination.currentPage}&limit=5`,
+          `${API_category}?sort=category name&search=${searchTerm}&page=${pagination.currentPge}&limit=5`,
           { headers: { Authorization: `Bearer ${token}` } }
         );
         setCategories(categoriesResponse.data.data);
-        setPagination(categoriesResponse.data.paginationResult);
+        setPagination({
+          currentPge: pagination.currentPge,
+          totalPages: categoriesResponse.data.paginationResult.numberOfPages,
+          totalRecords: categoriesResponse.data.paginationResult.totalRecords,
+        });
       } else {
         console.error("No token found.");
       }
@@ -46,11 +43,12 @@ const CategoryTable = ({ openEdit, openCreate, openPreview }) => {
     } finally {
       setLoading(false);
     }
-  }, [token, searchTerm, pagination.currentPage]);
+  }, [token, searchTerm, pagination.currentPge]);
+
 
   useEffect(() => {
     fetchData();
-  }, [searchTerm, pagination.currentPage, fetchData]);
+  }, [searchTerm, pagination.currentPge, fetchData]);
 
   const handleDeleteCategory = (categoryId) => {
     setSelectedCategoryId(categoryId);
@@ -78,7 +76,7 @@ const CategoryTable = ({ openEdit, openCreate, openPreview }) => {
   const handlePageChange = (newPage) => {
     setPagination((prevPagination) => ({
       ...prevPagination,
-      currentPage: newPage,
+      currentPge: newPage,
     }));
   };
 
@@ -93,18 +91,6 @@ const CategoryTable = ({ openEdit, openCreate, openPreview }) => {
     openEdit(category);
   };
 
-  const handlePreviousPage = () => {
-    if (pagination.currentPage > 1) {
-      handlePageChange(pagination.currentPage - 1);
-    }
-  };
-
-  const handleNextPage = () => {
-    if (pagination.currentPage < pagination.totalPages) {
-      handlePageChange(pagination.currentPage + 1);
-    }
-  };
-
   const toggleEditDropdown = (categoryId) => {
     setSelectedCategoryId((prevCategoryId) =>
       prevCategoryId === categoryId ? null : categoryId
@@ -113,17 +99,26 @@ const CategoryTable = ({ openEdit, openCreate, openPreview }) => {
 
   const dropdownRefs = useRef({});
 
-  const MAX_DISPLAY_PAGES = 5; // Define the maximum number of displayed pages
+  const MAX_DISPLAY_PAGES = 5;
 
-  const startPage = Math.max(1, pagination.currentPage - Math.floor(MAX_DISPLAY_PAGES / 2));
+  const startPage = Math.max(1, Math.min(pagination.currentPge - Math.floor(MAX_DISPLAY_PAGES / 2), pagination.totalPages - MAX_DISPLAY_PAGES + 1));
   const endPage = Math.min(startPage + MAX_DISPLAY_PAGES - 1, pagination.totalPages);
 
   const pageButtons = Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index);
+  const handlePreviousPage = () => {
+    if (pagination.currentPge > 1) {
+      handlePageChange(pagination.currentPge - 1);
+    }
+  };
 
-
+  const handleNextPage = () => {
+    if (pagination.currentPge < pagination.totalPages) {
+      handlePageChange(pagination.currentPge + 1);
+    }
+  };
 
   return (
-    <section className=" bg-gray-700 bg-opacity-25 mx-10 rounded-md pt-2 absolute top-40 w-3/4 ">
+    <section className="bg-gray-700 bg-opacity-25 mx-10 rounded-md pt-2 absolute top-40 w-3/4">
       <ConfirmationModal
         show={showConfirmation}
         onCancel={cancelDelete}
@@ -133,32 +128,30 @@ const CategoryTable = ({ openEdit, openCreate, openPreview }) => {
         }}
       />
       <div className="flex justify-between">
-        {" "}
         <div className="relative w-96 m-3">
-          {" "}
           <input
             className="px-4 py-2 pl-10 rounded-lg border border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-700 bg-gray-500"
             type="text"
             onClick={handleSearch}
             placeholder={t("Products.Search")}
-          />{" "}
+          />
           <CiSearch
             className={`absolute top-2 text-white text-xl ${language === "ar" ? "left-3" : "right-3"
               } `}
-          />{" "}
+          />
         </div>
         <div>
           <button
             className="bg-yellow-900 w-28 rounded-md m-3 hover:bg-yellow-800 fw-bold"
             onClick={openCreate}
           >
-            {t("Products.Add")}{" "}
+            {t("Products.Add")}
           </button>
         </div>
       </div>
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className="text-xm text-gray-200 uppercase">
-          <tr className="text-center fs-6 bg-gray-500 tracking-wide  bg-opacity-25 transition ease-out duration-200">
+          <tr className="text-center fs-6 bg-gray-500 tracking-wide bg-opacity-25 transition ease-out duration-200">
             <th scope="col" className="px-5 py-4">
               {t("Category.Code")}
             </th>
@@ -173,7 +166,7 @@ const CategoryTable = ({ openEdit, openCreate, openPreview }) => {
         <tbody>
           {loading ? (
             <tr>
-              <td colSpan="3" className=" fs-4 text-center mb-5 pb-3">
+              <td colSpan="3" className="fs-4 text-center mb-5 pb-3">
                 <Loading />
               </td>
             </tr>
@@ -191,16 +184,14 @@ const CategoryTable = ({ openEdit, openCreate, openPreview }) => {
                 >
                   <th
                     scope="row"
-                    className="px-4 py-4 font-medium text-gray-900whitespace-nowrap dark:text-white max-w-[5rem] truncate"
+                    className="px-4 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white max-w-[5rem] truncate"
                   >
-                    {" "}
                     {category._id.slice(-4)}
-                    {/* {console.log(category)} */}
                   </th>
                   <td className="px-4 py-4">{category.name}</td>
-                  <td className="px-4 py-3 flex items-center justify-end" >
+                  <td className="px-4 py-3 flex items-center justify-end">
                     <button
-                      className="inline-flex items-center text-sm font-medium   p-1.5  text-center text-gray-500 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100 bg-transparent"
+                      className="inline-flex items-center text-sm font-medium p-1.5 text-center text-gray-500 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100 bg-transparent"
                       type="button"
                       onClick={() => toggleEditDropdown(category._id)}
                       ref={(el) => (dropdownRefs.current[category._id] = el)}
@@ -208,7 +199,7 @@ const CategoryTable = ({ openEdit, openCreate, openPreview }) => {
                       <DotsThree
                         size={25}
                         weight="bold"
-                        className=" hover:bg-gray-700 w-10 rounded-lg"
+                        className="hover:bg-gray-700 w-10 rounded-lg"
                       />
                     </button>
                     <div
@@ -226,7 +217,7 @@ const CategoryTable = ({ openEdit, openCreate, openPreview }) => {
                           <li className="">
                             <button
                               type="button"
-                              className="flex w-full items-center gap-3 fs-6 fw-bold justify-content-start py-2 px-4 bg-gray-700 hover:bg-gray-600  dark:hover:text-white text-gray-700 dark:text-gray-200"
+                              className="flex w-full items-center gap-3 fs-6 fw-bold justify-content-start py-2 px-4 bg-gray-700 hover:bg-gray-600 dark:hover:text-white text-gray-700 dark:text-gray-200"
                               onClick={() => handleEditProduct(category)}
                             >
                               <NotePencil size={18} weight="bold" />
@@ -236,7 +227,7 @@ const CategoryTable = ({ openEdit, openCreate, openPreview }) => {
                           <li>
                             <button
                               type="button"
-                              className="flex w-44 items-center gap-3 fs-6 fw-bold justify-content-start py-2 px-4 bg-gray-700 hover:bg-gray-600  dark:hover:text-white text-gray-700 dark:text-gray-200"
+                              className="flex w-44 items-center gap-3 fs-6 fw-bold justify-content-start py-2 px-4 bg-gray-700 hover:bg-gray-600 dark:hover:text-white text-gray-700 dark:text-gray-200"
                             >
                               <Eye size={18} weight="bold" />
                               {t("Category.Preview")}
@@ -245,11 +236,10 @@ const CategoryTable = ({ openEdit, openCreate, openPreview }) => {
                           <li>
                             <button
                               type="button"
-                              className="flex w-44 items-center gap-3 fs-6 fw-bold justify-content-start py-2 px-4 bg-gray-700 hover:bg-gray-600  dark:hover:text-white text-gray-700 dark:text-gray-200"
+                              className="flex w-44 items-center gap-3 fs-6 fw-bold justify-content-start py-2 px-4 bg-gray-700 hover:bg-gray-600 dark:hover:text-white text-gray-700 dark:text-gray-200"
                               onClick={() => handleDeleteCategory(category._id)}
                             >
                               <TrashSimple size={18} weight="bold" />
-
                               {t("Category.Delete")}
                             </button>
                           </li>
@@ -267,8 +257,8 @@ const CategoryTable = ({ openEdit, openCreate, openPreview }) => {
         <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
           {t("Products.appear")}{" "}
           <span className="font-semibold text-gray-900 dark:text-white m-2" dir="ltr">
-            {pagination.currentPage}-{Math.min(
-              pagination.currentPage * 10,
+            {pagination.currentPge}-{Math.min(
+              pagination.currentPge * 5,
               pagination.totalRecords
             )}{" "}
           </span>
@@ -290,9 +280,9 @@ const CategoryTable = ({ openEdit, openCreate, openPreview }) => {
           {pageButtons.map((page) => (
             <li key={page}>
               <button
-                className={`flex items-center justify-center text-sm py-2 px-3 leading-tight ${pagination.currentPage === page
-                    ? "bg-gray-200 text-gray-800"
-                    : "text-gray-500 bg-gray-700 border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                className={`flex items-center justify-center text-sm py-2 px-3 leading-tight ${pagination.currentPge === page
+                  ? "bg-gray-200 text-gray-800"
+                  : "text-gray-500 bg-gray-700 border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                   }`}
                 onClick={() => handlePageChange(page)}
               >
