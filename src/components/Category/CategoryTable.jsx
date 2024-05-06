1163c81b42ba48023795d3cd57e3b111a31bb73a
mahmoud140106 <amer140106@gmail.com>
2024-05-06 04:15:25 +0300

    preview category end conflict

--- src/components/Category/CategoryTable.jsx
+++ src/components/Category/CategoryTable.jsx
@@ -317,7 +317,7 @@ const CategoryTable = ({ openEdit, openCreate, openPreview }) => {
                 {page}
               </button>
             </li>
-          ))}
+          ))}   
           <li>
             <button
               className="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-gray-700 rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
