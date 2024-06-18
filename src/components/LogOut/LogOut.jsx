1c6be764f4314c74764e77e77a8cf463f99bb838
Ahmed-mohamed11 <ahmedmohamed0aa349@gmail.com>
2024-06-18 21:35:47 +0300

    updates

--- src/components/LogOut/LogOut.jsx
+++ src/components/LogOut/LogOut.jsx
@@ -2,23 +2,29 @@ import React from 'react';
 import './Logout.css';
 import Cookies from 'js-cookie';
 import { User } from '@phosphor-icons/react';
+import { useI18nContext } from '../context/i18n-context';
 
-const logOut = () => {
+const LogOut = () => {
+  const { t } = useI18nContext();
 
-  const handle = () => {
+  const handleLogOut = () => {
     Cookies.remove('token');
     window.location.href = '/';
   };
 
   return (
-    <div>
-      <User className='relative bg-transparent rounded-full p-1
-                     text-gray-500  dark:hover:text-white focus:outline-none
-                    hover:text-slate-500 w-fit z-50'
-        size={32} onClick={handle}
+    <div
+      title={t("Home.LogOut")}
+    >
+      <User
+        className='relative bg-transparent rounded-full p-1
+                   text-gray-500 dark:hover:text-white focus:outline-none
+                   hover:text-slate-500 w-fit z-50 cursor-pointer'
+        size={32}
+        onClick={handleLogOut}
       />
     </div>
   );
 };
 
-export default logOut; 
+export default LogOut;
--- src/components/Navbar/Header/header.js
+++ src/components/Navbar/Header/header.js
@@ -1,40 +1,49 @@
 import { useState } from "react";
 import { FaMoon, FaSun } from "react-icons/fa";
-import "../../../App.css"
+import "../../../App.css";
+import { useI18nContext } from "../../context/i18n-context";
+
 function Header() {
-  const [show, setshow] = useState(true)
+  const [show, setShow] = useState(true);
+
   const mode = () => {
-    setshow(!show)
+    setShow(!show);
     if (window.localStorage.getItem("mode") === "light") {
       document.body.classList.add('light');
-      window.localStorage.setItem("mode", "dark")
-      console.log(localStorage.getItem('mode'))
-    }
-    else if (window.localStorage.getItem("mode") === "dark") {
+      window.localStorage.setItem("mode", "dark");
+      console.log(localStorage.getItem('mode'));
+    } else if (window.localStorage.getItem("mode") === "dark") {
       document.body.classList.remove('light');
-      window.localStorage.setItem("mode", "light")
-      console.log(localStorage.getItem('mode'))
+      window.localStorage.setItem("mode", "light");
+      console.log(localStorage.getItem('mode'));
     }
-  }
+  };
 
   if (window.localStorage.getItem("mode")) {
     document.body.classList = localStorage.getItem("mode");
+  } else {
+    window.localStorage.setItem("mode", "dark");
   }
-  else {
-    window.localStorage.setItem("mode", "dark")
-  }
+
+  const { t } = useI18nContext();
 
   return (
     <>
       <header className="flex z-50">
-        <div id="icon"  onClick={mode}>
-          {window.localStorage.getItem("mode") === "light" ? <FaMoon 
-           className="relative bg-transparent rounded-full p-1  
-                    text-gray-500  dark:hover:text-white focus:outline-none
-                    hover:text-slate-500 w-fit"  size={32}/> : <FaSun
-                     className="relative bg-transparent rounded-full p-1  
-                     text-gray-500  dark:hover:text-white focus:outline-none
-                    hover:text-slate-500 w-fit"  size={32}/>}
+        <div id="icon" onClick={mode}>
+          {window.localStorage.getItem("mode") === "light" ? (
+            <FaMoon
+              className="relative bg-transparent rounded-full p-1 text-gray-500 dark:hover:text-white focus:outline-none hover:text-slate-500 w-fit"
+              size={32}
+              title={t("Home.Mode")}
+            />
+          ) : (
+            <FaSun
+              className="relative bg-transparent rounded-full p-1 text-gray-500 dark:hover:text-white focus:outline-none hover:text-slate-500 w-fit"
+              size={32}
+              title={t("Home.Mode")}
+            />
+          )}
         </div>
       </header>
     </>
