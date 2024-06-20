163c18264fe28543710e7eab4fb6131458b5bbcc
dinamagdyrashed <dinarashid78@gmail.com>
2024-06-21 01:14:01 +0300

    list

---
+++ src/components/BestSeller/CartHover.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+const CartHover = () => {
+    return (
+        <div>
+            <div>
+                You have x number of items in your cart and here they are
+                <br />
+                Item 1
+                <br />
+                Item 2
+            </div>
+        </div>
+    )
+}
+export default CartHover
--- src/components/BestSeller/Shops.jsx
+++ src/components/BestSeller/Shops.jsx
@@ -4,15 +4,19 @@ import axios from 'axios';
 import Loading from '../Loading/Loading';
 import { useI18nContext } from "../context/i18n-context";
 import { useNavigate } from 'react-router-dom';
-import { ErrorAlert } from '../../form/Alert'; // Adjust the import path accordingly
+import { ErrorAlert } from '../../form/Alert';
+import { IoIosArrowDropleft } from "react-icons/io";
+import './styles.css';
 
 const Shops = () => {
     const API_URL = "https://store-system-api.gleeze.com/api/shops";
+    const API_URL_Type = "https://store-system-api.gleeze.com/api/shopTypes/list";
     const [shops, setShops] = useState([]);
+    const [shopTypes, setShopTypes] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const [searchTerm, setSearchTerm] = useState("");
-    const [selectedTypes, setSelectedTypes] = useState([]);
+    const [selectedType, setSelectedType] = useState(null);
     const token = Cookies.get("token");
     const { t, language } = useI18nContext();
     const navigate = useNavigate();
@@ -20,35 +24,42 @@ const Shops = () => {
     const fetchData = useCallback(async () => {
         setLoading(true);
         try {
-            const typeFilter = selectedTypes.length > 0 ? `&type=${selectedTypes.join("&")}` : "";
+            const typeFilter = selectedType ? `type=${selectedType}` : "";
+            const query = `sort=name&search=${searchTerm}&${typeFilter}`;
             const productsResponse = await axios.get(
-                `${API_URL}?sort=-sold name&search=${searchTerm}${typeFilter}`,
+                `${API_URL}?${query}`,
                 { headers: { Authorization: `Bearer ${token}` } }
             );
             setShops(productsResponse.data.data);
-            setError(null); // Clear any previous errors
+            setError(null);
         } catch (error) {
             setError(error.response?.data?.message || "Error fetching data");
             ErrorAlert({ text: error.response?.data?.message || "Error fetching data" });
         } finally {
             setLoading(false);
         }
-    }, [token, searchTerm, selectedTypes]);
+    }, [token, searchTerm, selectedType]);
+
+    const fetchShopTypes = useCallback(async () => {
+        try {
+            const typesResponse = await axios.get(API_URL_Type, {
+                headers: { Authorization: `Bearer ${token}` }
+            });
+            setShopTypes(typesResponse.data.data);
+        } catch (error) {
+            setError(error.response?.data?.message || "Error fetching shop types");
+            ErrorAlert({ text: error.response?.data?.message || "Error fetching shop types" });
+        }
+    }, [token]);
 
     useEffect(() => {
         fetchData();
-    }, [fetchData]);
+        fetchShopTypes();
+    }, [fetchData, fetchShopTypes]);
 
-    const handleTypeChange = (type) => {
-        setSelectedTypes(prevSelectedTypes =>
-            prevSelectedTypes.includes(type)
-                ? prevSelectedTypes.filter(t => t !== type)
-                : [...prevSelectedTypes, type]
-        );
+    const handleTypeChange = (typeId) => {
+        setSelectedType(typeId);
     };
-
-    // Extract unique types for the checkboxes
-    const uniqueTypes = Array.from(new Set(shops.flatMap(shop => shop.type.map(t => t.type_en))));
 
     return (
         <div>
@@ -60,7 +71,7 @@ const Shops = () => {
                     ) : (
                         <div className='flex flex-wrap gap-x-4 gap-y-6'>
                             {shops.map((shop) => (
-                                <div key={shop._id} className='bg-gray-500 p-2 w-72 h-80 bg-opacity-25 rounded-xl overflow-hidden relative m-3'>
+                                <div key={shop._id} className='bg-gray-500 p-2 w-72 h-96 bg-opacity-25 rounded-xl overflow-hidden relative m-3'>
                                     <img
                                         src={shop.image}
                                         alt={shop.name}
@@ -69,7 +80,12 @@ const Shops = () => {
                                     />
                                     <div>
                                         <h3 className='mt-2 text-white font-bold text-center'>{shop.name}</h3>
-                                        <h4 className='mt-2 text-white font-bold text-center'>Type: {language === "ar" ? shop.type[0].type_ar : shop.type[0].type_en}</h4>
+                                        <h4 className='mt-2 text-white font-bold text-center'>
+                                            Type:
+                                        </h4>
+                                        <h4 className='mt-2 text-white font-bold text-center'>
+                                            {shop.type.map((type) => language === "ar" ? type.type_ar : type.type_en).join(', ')}
+                                        </h4>
                                     </div>
                                     <div className='flex justify-center mb-5 mx-2'>
                                         <button
@@ -85,21 +101,40 @@ const Shops = () => {
                     )}
                 </div>
             </section>
-            <div className='bg-gray-700 bg-opacity-25 h-2/3 w-44 absolute top-32 right-0 p-3'>
-                <h3 className="font-bold text-white text-xl mb-3">Filter by Type</h3>
-                {uniqueTypes.map((type) => (
-                    <div key={type} className="mb-2">
-                        <label className="text-white flex w-2/3 ">
+            <div className='filter-container bg-gray-700 bg-opacity-25 w-44 absolute top-32 p-3 '>
+                <div className='flex w-full'>
+                    <IoIosArrowDropleft className='text-white text-3xl mr-2' />
+                    <div>
+                        <h3 className="font-bold text-white text-xl mb-3">Filter by Type</h3>
+                        <div className="mb-2 pr-4 flex w-2/3">
                             <input
-                                type="checkbox"
-                                checked={selectedTypes.includes(type)}
-                                onChange={() => handleTypeChange(type)}
-                                className='mx-2'
+                                type="radio"
+                                name="typeFilter"
+                                id='all'
+                                checked={selectedType === null}
+                                onChange={() => handleTypeChange(null)}
                             />
-                            {type}
-                        </label>
+                            <label className="text-white w-10" htmlFor='all'>
+                                All
+                            </label>
+                        </div>
+                        {shopTypes.map((type) => (
+                            <div key={type._id} className="mb-2 flex w-2/3">
+                                <input
+                                    type="radio"
+                                    name="typeFilter"
+                                    id={type.type_en}
+                                    checked={type._id === selectedType}
+                                    onChange={() => handleTypeChange(type._id)}
+                                    className='m-0'
+                                />
+                                <label className="text-white w-10 text-capitalize" htmlFor={type.type_en}>
+                                    {language === "ar" ? type.type_ar : type.type_en}
+                                </label>
+                            </div>
+                        ))}
                     </div>
-                ))}
+                </div>
             </div>
         </div>
     );
